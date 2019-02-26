f06699f19e8385fd71d20f60f523068797b094da
root <root@localhost.localdomain>
2019-02-26 15:05:31 +0000

    Deleting some files

--- valligator/simple_validators.py
+++ valligator/simple_validators.py
@@ -28,11 +28,28 @@ def check_bug_trackers(cover, patches, key):
     return to_return
 
 
+def ntimes_keyword(cover, key):
+    """ Number of time a word appears in the text """
+    pos = 0
+    ntimes_key = 0
+    new_init = 0
+
+    while pos != -1:
+        pos = cover[new_init:].find(key) 
+        if pos != -1:
+            ntimes_key += 1
+            new_init += pos+len(key)
+        else:
+            return ntimes_key
+
+
 def subject_contains_keyword(cover, patches, key):
     """ Check the subject contains a required keyword  """
 
     subject_content = ""
     to_return = False
+
+    print("The term 'Subject:' appears " + str(ntimes_keyword(cover, "Subject:")) + " times in the cover letter")
 
     cover_lines = cover.split('\n')
     for line in cover_lines:
@@ -44,9 +61,9 @@ def subject_contains_keyword(cover, patches, key):
             to_return = True
 
     if to_return:
-        print("Keyword " + key + " found in subject content")
+        print("Keyword " + key + " found in 'Subject:' content")
     else:
-        print("Keyword " + key + " not found in subject content")
+        print("Keyword " + key + " not found in 'Subject:' content")
 
 
     return to_return
